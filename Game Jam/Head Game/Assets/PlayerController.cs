279aa1bbd22df21e8cd055b7541289ac24e9adaa
Mike Ford <michaelfordaz@yahoo.com>
2021-11-05 20:52:19 -0700

    I Made a Ball

--- Game Jam/Head Game/Assets/PlayerController.cs
+++ Game Jam/Head Game/Assets/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float MovementSpeed;
-    private void Start()
+    Rigidbody2D m_Rigidbody;
+    public float m_Thrust = 20f;
+
+    public float horizontalInput;
+
+    void Start()
     {
-     
+        
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        
+      
     }
 
     void Update()
     {
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        horizontalInput = Input.GetAxis("Horizontal");          
+    }
+
+    void FixedUpdate()
+    {
+        m_Rigidbody.AddForce(new Vector3(horizontalInput, 0.0f, 0) * m_Thrust);
     }
 }
